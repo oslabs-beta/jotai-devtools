3c3eefd65f16d8c455e365808587123727ee1e1a
tori-obregon <vobregon@gmail.com>
2024-03-21 12:29:36 -0700

    attempt at creating custom cssModulePlugin to solve css leaking

--- JotaiDevToolV/tsup.config.ts
+++ JotaiDevToolV/tsup.config.ts
@@ -1,5 +1,9 @@
 import { replace } from 'esbuild-plugin-replace';
 import { Options, defineConfig } from 'tsup';
+import fsPromises from 'fs/promises';
+import path from 'path';
+import postcss from 'postcss';
+import postcssModules from 'postcss-modules';
 
 
 const defaultOutExtension: Options['outExtension'] = ({ format }) => {
@@ -8,12 +12,79 @@ const defaultOutExtension: Options['outExtension'] = ({ format }) => {
   };
 };
 
+const cssModulePlugin = {
+  name: "css-module",
+  setup(build): void {
+          build.onResolve(
+            { filter: /\.module\.css$/, namespace: "file" },
+            (args) => {
+              const pathDir = path.join(__dirname, args.path);
+              // Log the original args and the constructed pathDir
+              console.log('onResolve args:', args);
+              console.log('Constructed pathDir:', pathDir);
+              return {
+                path: `${args.path}`,
+                namespace: "css-module",
+                pluginData: {
+                  pathDir: pathDir,
+                },
+              };
+            });
+          
+          build.onLoad(
+            { filter: /#css-module$/, namespace: "css-module" },
+            async (args) => {
+              const { pluginData } = args as {
+                pluginData: { pathDir: string };
+              };
+
+              const source = await fsPromises.readFile(
+                pluginData.pathDir,
+                "utf8"
+              );
+
+              let cssModule = {};
+              const result = await postcss([
+                postcssModules({
+                  getJSON(_, json) {
+                    cssModule = json;
+                  },
+                }),
+              ]).process(source, { from: pluginData.pathDir });
+
+              return {
+                pluginData: { css: result.css },
+                contents: `import "${
+                  pluginData.pathDir
+                }"; export default ${JSON.stringify(cssModule)}`,
+              };
+            }
+          );
+          build.onResolve(
+            { filter: /\.module\.css$/, namespace: "css-module" },
+            (args) => ({
+              path: path.join(args.resolveDir, args.path),
+              namespace: "css-module",
+              pluginData: args.pluginData as { css: string },
+            })
+          );
+          build.onLoad(
+            { filter: /#css-module-data$/, namespace: "css-module" },
+            (args) => ({
+              contents: (args.pluginData as { css: string }).css,
+              loader: "css",
+            })
+          );
+        },
+};
+
 const defaultEsBuildPlugins: Options['esbuildPlugins'] = [
   replace({
     // FIXME - Should filter it by `include` instead of `exclude`. This doesn't seem to be working /^.*\.js$/,
     exclude: /\.woff2$/,
     __DEV__: '(process.env.NODE_ENV!=="production")',
   }),
+  cssModulePlugin,
 ];
 
 const baseConfig: Options = {
@@ -26,7 +97,6 @@ const baseConfig: Options = {
   },
   loader: {
     '.woff2': 'dataurl',
-    '.css': 'local-css',
   },
   sourcemap: true,
   // Clean output directory before each build
