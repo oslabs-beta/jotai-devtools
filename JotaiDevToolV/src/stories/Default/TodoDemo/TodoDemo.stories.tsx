0eaa2ca770df362c3544da19ae8812f994943f30
tori-obregon <vobregon@gmail.com>
2024-03-21 14:29:18 -0700

    TodoApp story added

---
+++ JotaiDevToolV/src/stories/Default/TodoDemo/TodoDemo.stories.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+import { DevTools, DevToolsProps } from '../../../';
+import { Provider } from 'jotai/react';
+import TodoApp from './TodoApp';
+
+export default {
+  component: DevTools,
+  title: 'Devtools + TodoApp',
+} as Meta<typeof DevTools>;
+
+type CustomStorybookProps = DevToolsProps & {
+  'options.shouldShowPrivateAtoms': boolean;
+};
+
+type Story = StoryObj<CustomStorybookProps>;
+
+export const Default: Story = {
+  render: ({ ...args }) => {
+    const nextOptions = {
+      ...args.options,
+      shouldShowPrivateAtoms: args['options.shouldShowPrivateAtoms'],
+    };
+    const props = {
+      ...args,
+      options: nextOptions,
+    };
+    return (
+      <Provider>
+          <DevTools {...props} />
+          <TodoApp {...props} />;
+      </Provider>
+    )
+  },
+  args: {
+    isInitialOpen: false,
+    'options.shouldShowPrivateAtoms': false,
+  },
+  argTypes: {
+    store: {
+      control: {
+        type: false,
+      },
+    },
+    options: {
+      control: {
+        type: false,
+      },
+    },
+  },
+};
--- JotaiDevToolV/tsup.config.ts
+++ JotaiDevToolV/tsup.config.ts
@@ -1,81 +1,10 @@
 import { replace } from 'esbuild-plugin-replace';
 import { Options, defineConfig } from 'tsup';
-import fsPromises from 'fs/promises';
-import path from 'path';
-import postcss from 'postcss';
-import postcssModules from 'postcss-modules';
-
 
 const defaultOutExtension: Options['outExtension'] = ({ format }) => {
   return {
     js: `.${format}.js`,
   };
-};
-
-const cssModulePlugin = {
-  name: "css-module",
-  setup(build): void {
-          build.onResolve(
-            { filter: /\.module\.css$/, namespace: "file" },
-            (args) => {
-              const pathDir = path.join(__dirname, args.path);
-              // Log the original args and the constructed pathDir
-              console.log('onResolve args:', args);
-              console.log('Constructed pathDir:', pathDir);
-              return {
-                path: `${args.path}`,
-                namespace: "css-module",
-                pluginData: {
-                  pathDir: pathDir,
-                },
-              };
-            });
-          
-          build.onLoad(
-            { filter: /#css-module$/, namespace: "css-module" },
-            async (args) => {
-              const { pluginData } = args as {
-                pluginData: { pathDir: string };
-              };
-
-              const source = await fsPromises.readFile(
-                pluginData.pathDir,
-                "utf8"
-              );
-
-              let cssModule = {};
-              const result = await postcss([
-                postcssModules({
-                  getJSON(_, json) {
-                    cssModule = json;
-                  },
-                }),
-              ]).process(source, { from: pluginData.pathDir });
-
-              return {
-                pluginData: { css: result.css },
-                contents: `import "${
-                  pluginData.pathDir
-                }"; export default ${JSON.stringify(cssModule)}`,
-              };
-            }
-          );
-          build.onResolve(
-            { filter: /\.module\.css$/, namespace: "css-module" },
-            (args) => ({
-              path: path.join(args.resolveDir, args.path),
-              namespace: "css-module",
-              pluginData: args.pluginData as { css: string },
-            })
-          );
-          build.onLoad(
-            { filter: /#css-module-data$/, namespace: "css-module" },
-            (args) => ({
-              contents: (args.pluginData as { css: string }).css,
-              loader: "css",
-            })
-          );
-        },
 };
 
 const defaultEsBuildPlugins: Options['esbuildPlugins'] = [
@@ -84,7 +13,6 @@ const defaultEsBuildPlugins: Options['esbuildPlugins'] = [
     exclude: /\.woff2$/,
     __DEV__: '(process.env.NODE_ENV!=="production")',
   }),
-  cssModulePlugin,
 ];
 
 const baseConfig: Options = {
