60378a0cc7c79405605f3a100865af9cc44cbda2
Adam Kim <adamkim1006@gmail.com>
2024-03-29 19:12:43 -0700

    feat(jotai-devtools): setting data-mantine-color-scheme attribute on outer html to light permanently

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,3 +1,27 @@
+import { addons } from '@storybook/addons';
+import { useEffect, createElement } from 'react';
+
+// Set the data-mantine-color-scheme attribute to "light"
+const setDataMantineColorSchemeLight = () => {
+  const rootElement = document.documentElement;
+  rootElement.setAttribute('data-mantine-color-scheme', 'light');
+  // Removal of the data-mantine-color-scheme attribute
+  // rootElement.removeAttribute('data-mantine-color-scheme')
+};
+
+// Add a decorator to set the data-mantine-color-scheme attribute on each story render
+addons.getChannel().on('STORY_RENDERED', setDataMantineColorSchemeLight);
+
+export const decorators = [
+  (storyComponent, context) => {
+    useEffect(() => {
+      setDataMantineColorSchemeLight();
+    }, []);
+
+    return createElement(storyComponent, context.args);
+  },
+];
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
